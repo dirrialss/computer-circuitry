a83536494d9f051fcb8e0464386ed3c23bf32cad
dirrialss <153391356+dirrialss@users.noreply.github.com>
2025-04-08 23:23:44 +0300

    Add files via upload

---
+++ lab4/work/work/comb2_@siukalo/_primary.vhd
@@ -0,0 +1,10 @@
+library verilog;
+use verilog.vl_types.all;
+entity comb2_Siukalo is
+    port(
+        x3              : in     vl_logic;
+        x2              : in     vl_logic;
+        x1              : in     vl_logic;
+        f4              : out    vl_logic
+    );
+end comb2_Siukalo;
